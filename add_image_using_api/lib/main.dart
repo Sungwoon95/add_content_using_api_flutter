a8877c601faa62b433126c1203bbba4ed70cb5d2
sungwoon95 <lad0305@naver.com>
2021-12-04 02:31:02 +0900

    feat: add image using api

---
+++ add_image_using_api/lib/main.dart
@@ -0,0 +1,14 @@
+// i need to import a helper library
+// from flutter to get content on the screen
+import 'package:flutter/material.dart';
+import 'src/app.dart';
+
+// Define a 'main' function to run when our app start
+void main() {
+  runApp(App());
+  // Create a new text widget to show some Text
+  // on the screen
+// Take that widget and get it on the screen
+
+}
+
